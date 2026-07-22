ae0abff28c347179101babc9444ab3e10169f221
Shaurya Singh <shauryasingh0302@icloud.com>
2026-07-23 04:31:35 +0530

    chore: initialize project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added image assets for the project
    - Initialized TypeScript configuration with tsconfig.json

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Space_Grotesk, JetBrains_Mono } from "next/font/google";
+// @ts-ignore: allow side-effect CSS import when type declarations are missing
+import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+});
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
+});
+
+const siteUrl =
+  process.env.VERCEL_ENV === "preview"
+    ? `https://${process.env.VERCEL_BRANCH_URL}`
+    : process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : "http://localhost:3000";
+
+const title = "Shaurya Singh";
+const description =
+  "shauryasingh0302";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteUrl),
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url: "./",
+    siteName: title,
+    images: "/og.jpg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning className="scroll-smooth shadcn">
+      <body
+        className={`font-body antialiased ${spaceGrotesk.variable} ${jetBrainsMono.variable}`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
